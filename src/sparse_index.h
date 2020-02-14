e4e5c42c0a81ae2d697ce309e4b16c82dd0eb4ce
SURYADEV <srsuryadev@gmail.com>
2020-02-14 17:49:31 -0600

    added sparse index class

---
+++ src/sparse_index.h
@@ -0,0 +1,56 @@
+#include <map>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class SparseIndex {
+
+
+    private:
+        map<string, int> sparse_index_;
+
+        std::ofstream out_index_;
+        std::ifstream in_index_;
+
+        string id_;
+        bool is_write_;
+
+    public:
+
+        SparseIndex(string id, bool is_write) {
+            this->id_ = id;
+            this->is_write_ = is_write;
+
+            if (this->is_write_) {
+                out_index_.open(id, std::ofstream::out);
+            } else {
+                in_index_.open(id, std::ifstream::in);
+            }
+        }
+
+        int GetOffset(string key) {
+            return sparse_index_.lower_bound(key)->second;
+        }
+
+        bool put(string key, int offset) {
+            sparse_index_[key] = offset;
+        }
+
+        void Read() {
+            in_index_.read((char *) this, sizeof(*this));
+        }
+
+        void Write() {
+            out_index_.write((char *) this, sizeof(*this));
+        }
+
+        ~SparseIndex() {
+             if (this->is_write_) {
+                out_index_.close();
+            } else {
+                in_index_.close();
+            }
+        }
+
+};
